b4b27b99ddbcf55496efd82f656ec393058690fb
rayat69 <rayathossain49@gmail.com>
2021-01-23 03:00:53 +0600

    added src and public

---
+++ template/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import {app, app_logo, app_header, app_link} from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={app}>
+      <header className={app_header}>
+        <img src={logo} className={app_logo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to see the magic.
+        </p>
+        <a
+          className={app_link}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
